8541c59aa84c83df670f718ac2740b071dbe2503
mh-utsho <136597128+mh-utsho@users.noreply.github.com>
2024-07-08 21:33:53 +0600

    Add files via upload
    old temp

---
+++ utsho_old.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll                  long long int
+#define F                   first
+#define S                   second
+#define pb                  push_back
+#define endl                "\n"
+#define mp                  make_pair
+// const int dx[] = {0,1,1, 1, 0,-1,-1,-1};
+// const int dy[] = {1,1,0,-1,-1,-1, 0, 1};
+//-----------
+#define all(x)              x.begin(), x.end()
+#define rall(x)             x.rbegin(),x.rend()
+#define forn(i, s, e)       for (ll i = (s); i < (e); i++)
+#define loop(e)             for (ll i = 1 ; i <= (e); i++)
+#define in(x)               for(auto &el:x) cin>>el
+#define out(x)              for(auto &el:x) cout<<el<<" "
+#define DBG(a)              cerr<< "Line "<<__LINE__ <<" : "<< #a <<" = "<<(a)<<endl
+#define fast                ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//------------
+#define print(x)            for(auto el:x) cout<<el<<" "
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define NL                  cout<<"\n"
+#define p(x)                cout<<x<<endl
+#define sq_check(x) (((ll)(sqrt((x))))*((ll)(sqrt((x))))==(x))
+ 
+//_______________________________________________________________________
+//ll D_T_B(ll n) {if(n==0)return 0;string s="";while(n>0){s+=(n%2==0?"0":"1");n/=2;}reverse(all(s));return stoll(s);}
+// ll power(ll a,ll b){ll an=1;while(b){if(b&1) an*=a;a*=a;b>>=1;}return an;}
+//_______________________________________________________________________
+
+
+
+
+void solve(){
+   ll a,b,k=0,m,n,ans=0;
+   string s,t; 
+      
+}
+ 
+ 
+int main(){
+    fast;
+    int T=1;
+    cin>>T;
+    while(T--) 
+    solve();
+    return 0;
+}
+
+
+
